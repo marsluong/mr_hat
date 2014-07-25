213036e3bb713c1be6c6a9615ede074f44c3cc17
marsluong <mariavyl_10491@yahoo.com>
2014-07-25 12:00:55 -0500

    Changes before the account dropdown messed up

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ MrHat::Application.routes.draw do
   resources :users
   resources :sessions, only: [:new, :create, :destroy]
   root 	'static_pages#home'
-  match '/signup', 		to: 'users#new',					via: 'get'
+  match '/signup', 		to: 'users#new',				via: 'get'
   match '/signin', 		to: 'sessions#new',				via: 'get'
   match '/signout', 	to: 'sessions#destroy',			via: 'delete'
   match '/hat',			to: 'static_pages#hat',			via: 'get'
--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -15,11 +15,12 @@ describe "Authentication" do
 		describe "with invalid information" do
 			before { click_button "Sign in" }
 
-#			it { should have_title('Sign in') }
+			it { should have_title('Sign in') }
 			it { should have_selector('div.alert.alert-error') }
 
+
 			describe "after visiting another page" do
-#				before { click_link "Home" }
+				before { click_link "Home" }
 
 				it { should_not have_selector('div.alert.alert-error') }
 			end
